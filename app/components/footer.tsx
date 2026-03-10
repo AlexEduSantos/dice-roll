bcee92474b053be55013284ab86bc75487bd9990
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2026-03-10 16:30:16 -0300

    feat:build and launch 2.1 version style: footer buttons minor adjusts and remove history scrollbar

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -18,27 +18,29 @@ export default function Footer({
     <div className="w-full min-h-14 h-fit p-3 flex items-center bg-background shadow-[0_-5px_20px_10px] shadow-background border-t-2 border-secondary fixed bottom-0">
       <div className="grid grid-cols-4 gap-3 items-center h-full w-full justify-between">
         <Button
-          variant="ghost"
+          variant={hasSelectedDice ? "ghost" : "outline"}
           onClick={onAdvantage}
-          className="flex flex-col items-center border-advantage/50 h-full w-full text-[10px] font-light uppercase tracking-wide text-muted-foreground"
+          disabled={!hasSelectedDice}
+          className={`flex flex-col items-center rounded-2xl h-full w-full text-[10px] font-light uppercase tracking-wide text-muted-foreground ${hasSelectedDice ? "shadow-[0_0px_10px_0px] shadow-advantage/20 text-advantage border-advantage/50" : "text-primary-foreground border-border"}`}
         >
-          <ArrowUp className="text-advantage" />
+          <ArrowUp />
           Vantagem
         </Button>
         <Button
           onClick={onRoll}
           variant={hasSelectedDice ? "default" : "outline"}
-          className={`col-span-2 min-h-18 rounded-2xl relative  text-2xl uppercase tracking-widest font-semibold ${hasSelectedDice ? "shadow-[0_0px_10px_0px] shadow-primary text-primary-foreground" : "text-primary"}`}
+          className={`col-span-2 min-h-18 rounded-2xl text-2xl uppercase tracking-widest  ${hasSelectedDice ? "shadow-[0_0px_10px_0px] shadow-primary text-primary-foreground font-bold" : "text-primary"}`}
         >
           <ZapIcon className="size-5" />
           Rolar
         </Button>
         <Button
-          variant="ghost"
+          variant={hasSelectedDice ? "ghost" : "outline"}
           onClick={onDisadvantage}
-          className="flex flex-col items-center border-disadvantage/50 h-full w-full text-[10px] font-light uppercase tracking-wide text-muted-foreground"
+          disabled={!hasSelectedDice}
+          className={`flex flex-col items-center rounded-2xl h-full w-full text-[10px] font-light uppercase tracking-wide text-muted-foreground ${hasSelectedDice ? "shadow-[0_0px_10px_0px] shadow-disadvantage/20 text-disadvantage border-disadvantage/50" : "text-primary-foreground border-border"}`}
         >
-          <ArrowDown className="text-disadvantage" />
+          <ArrowDown />
           Desvantagem
         </Button>
       </div>
